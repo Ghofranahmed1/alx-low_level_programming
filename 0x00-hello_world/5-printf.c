cf3d54a09889cccedc0b66846b24b28a858bc978
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-07 19:12:38 -0700

    101-quote.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,12 @@
-/*
- * File: 5-printf.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
+ * main - ENTRY POINT
  *
- * Return: Always 0.
+ * return always 0
  */
-int main(void)
+int main<void>
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-}  
+}
