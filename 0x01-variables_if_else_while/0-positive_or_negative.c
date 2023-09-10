917fdce58ac53628aeb8b831691840f929afbbea
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-10 10:03:01 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include<stdio.h>
-/**
-* main - entry piont
+/*
+* main - Entry piont
 *
 * return:always 0 (success)
 */
@@ -16,7 +16,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else 
+	else
 		printf("%d is negative\n", n);
 	return (0);
 }
