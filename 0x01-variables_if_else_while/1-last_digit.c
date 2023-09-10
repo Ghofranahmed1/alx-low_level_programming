0191c0e165095b090926bf3e30d7514eea1ba9cb
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-10 10:49:08 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,11 @@
 #include <time.h>
 #include<stdio.h>
 
-/*
+/**
 *main - Entry point
 *
+*the proggtam is to find the value of last gigit in a given number
+
 *return:always 0 (suuess)
 *
 */
@@ -16,7 +18,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
-		printf("Last digit of %d is %d  and is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
