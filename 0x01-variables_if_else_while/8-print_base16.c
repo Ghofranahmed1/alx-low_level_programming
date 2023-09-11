76ecca800c3c55d3ce0e7f5698d4a579036508b7
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-11 16:05:11 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*program print 16's alphabet bases
+*
+*Return: Always 0 (success)
+*/
+
+char c;
+int i;
+
+int main(void)
+{
+	for (i = '0'; i = '9'; i++)
+	putchar(i);
+	for (c = 'a'; c = 'z'; c++)
+	putchar(c);
+        putchar('\n');
+	
+	return (0);
+}
