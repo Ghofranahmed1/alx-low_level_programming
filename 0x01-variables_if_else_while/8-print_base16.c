c8d8110aab52d57e15401e7174bf8765c629d7a2
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-11 16:09:31 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,16 @@
 *Return: Always 0 (success)
 */
 
-char c;
-int i;
-
 int main(void)
 {
+	char c;
+        int i;
+
 	for (i = '0'; i <= '9'; i++)
 	putchar(i);
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	putchar(c);
         putchar('\n');
 	
-	return (0);
+return (0);
 }
