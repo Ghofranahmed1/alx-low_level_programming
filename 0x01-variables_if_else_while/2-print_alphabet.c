0c10dc08f615b71f8951d1416fa205b5d9f98538
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-11 08:15:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Enter point
+*
+*program to print all lower cases
+*
+*Return: always 0 (sucsess)
+*
+*/
+int main(void)
+{
+	for(char c = 'a' ; c <= 'z' ; c++)
+{
+	putchar('c');
+}
+	putchar('\n');
+          return (0);
+}       	
