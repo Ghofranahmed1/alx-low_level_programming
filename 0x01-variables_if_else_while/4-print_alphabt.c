37309d76527b5ca829bed8be2a35a1e1ea09ba7c
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-11 10:52:06 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	int c, e, q;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{		
+		if (c != 'e' && c != 'q')
+		putchar  (c);
+	}
+          	putchar ('\n');
+
+                return (0);
+}
