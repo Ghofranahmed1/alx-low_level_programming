d47bf9e19fc657d9915a932e937336204ac94799
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-11 17:31:16 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{ 
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a+1; b <= '9'; b++)
+		{
+			for (c = b+1; c <= '9'; c++)
+			{
+				if (b != a && a != c && c != b)
+				putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (b == '8' && c == '9')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
