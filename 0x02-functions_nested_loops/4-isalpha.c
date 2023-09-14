1e7690e9b30cd92259674aa3a25d2e00e6a96f0f
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-13 17:34:19 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,16 @@
 /**
  * _isalpha - check main.h
  *
+ * @c: An input character
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ *
  * Return: 1 if is lowercase and uppercase
  */
 
 int _isalpha(int c)
 {
-	if (( c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
