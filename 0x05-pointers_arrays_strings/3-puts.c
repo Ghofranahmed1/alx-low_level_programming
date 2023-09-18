f5708a9fb830025e379d211446d0bc67d1894fd6
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-18 12:36:32 -0700

    do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,16 +10,10 @@ void _puts(char *str)
 {
 int i = 0;
 
-while (str[i] != 0)
-{
-if (str[i] != 0)
+while (str[i] != '\0')
 {
 _putchar(str[i]);
-}
-else
-{
-_putchar('\n');
-}
 i++;
 }
+_putchar('\n');
 }
