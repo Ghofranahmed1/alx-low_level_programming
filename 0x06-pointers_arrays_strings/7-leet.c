5e6d0e7a62aef64b4681bb37969edcdf99aca147
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-24 14:59:38 -0700

    h

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @t: an input string to encode
+ * Return: An encode string
+ */
+char *leet(char *t)
+{
+int j, i;
+
+char letter[10] = {'a', 'A', 'e', 'E', 'O', 'O', 't', 'T', 'l', 'L'};
+char num[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+while (t[i])
+{
+for (j = 0; j < 10; j++)
+{
+if (letter[j] == t[i])
+{
+t[i] = num[j];
+}
+}
+i++;
+}
+return (t);
+}
