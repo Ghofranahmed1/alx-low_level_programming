f2b3d1ff5e1c0fd3350c6ef01b04ea5f2960db9a
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-09-24 17:05:12 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,13 @@ char *rot13(char *s)
 {
 int i = 0;
 
-while (s[i] != '\0') 
+while (s[i]) 
 {
 while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 {
 if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-{
 s[i] += 13;
-}
+else
 s[i] -= 13;
 i++;
 }
