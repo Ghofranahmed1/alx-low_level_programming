81d59a768e2b2f4df0e583a85783b69d885feda8
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-02 14:51:30 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,19 @@ int main(int argc, char **argv)
 {
 	int result = 0, num, i;
 
-	while (argc-- > 1)
-	{
-		for (i = 0; argv[argc][i]; i++)
-		{
-			if (!(isdigit(argv[argc][i])))
-			{
-			 printf("Error\n");
-			 return (1);
-			 }
-		}
+while (argc-- > 1)
+{
+for (i = 0; argv[argc][i]; i++)
+{
+if (!(isdigit(argv[argc][i])))
+{
+printf("Error\n");
+return (1);
+}
+}
 		num = atoi(argv[argc]);
 			result += num;
 	}
 	printf("%d\n", result);
-	return(0);
+	return (0);
 }
