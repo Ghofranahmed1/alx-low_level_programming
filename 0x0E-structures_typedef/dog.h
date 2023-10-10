b31c82e2fb2a452b20eedc794ceb4287e7d0f160
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-10 10:26:04 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+/**
+ * struct dog - new type define for gogs archives
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name og dog's owner
+ * Description: data structure for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
