843fe383807cca722fbddf1be0dd5f751be6fe33
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-10 15:54:09 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,8 +47,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+	newdog->name = malloc(sizeof(char) * (len1 + 1));
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
 	newdog->name = name;
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (newdog->owner == NULL)
+	{
+		free(newdog);
+			free(newdog->name);
+	}
 	return (newdog);
 }
