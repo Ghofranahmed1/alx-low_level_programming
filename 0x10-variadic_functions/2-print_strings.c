cae07703a1b5fa36ac9c79f841f6b72c91cc4711
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-12 12:14:24 -0700

    d

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("null");
 
 		printf("%s", s);
-		if (i < (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
