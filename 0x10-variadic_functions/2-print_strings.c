ecba1047d8a3566e99a5e7a9d04eda639baca1eb
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-12 19:33:13 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list str;
 
 	va_start(str, n);
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s = va_arg(str, char*);
 		if (s == NULL)
