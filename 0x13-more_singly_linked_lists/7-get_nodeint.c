dc8e2bac0edf2f8890d78a5fa339e6b0959355ec
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-24 11:26:34 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,23 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp = head;
-	unsigned int i = 0;
+	unsigned int i = 0, len = 0;
 
+	while (temp != NULL)
+	{
+		len++;
+		temp = temp->next;
+	}
+	temp = head;
+/* case if there only one node */
 	if (index == 0 && head->next == NULL)
 	{
 		return (head);
 	}
+	/* case the node dosen't exist */
+	else if (index > len)
+		return (NULL);
+
 	else
 	{
 	while (i < index)
