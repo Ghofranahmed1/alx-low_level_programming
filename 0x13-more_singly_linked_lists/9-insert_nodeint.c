4e6e20a7d1e545e0fc1310e32ea18b60c7164189
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-24 12:23:24 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 	while (temp != NULL)
 	{
 		len_node++;
@@ -25,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	if (idx > len_node)
 	{
+		free(new);
 		return (NULL);
 	}
 	while (i < (idx - 1))
