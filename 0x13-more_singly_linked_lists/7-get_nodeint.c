6c1aac1294f7097304ac81a797e0363a11a0d6f2
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-25 13:06:26 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i = 0, len = 0;
 
+	if(head == NULL)
+		return (NULL);
 	while (temp != NULL)
 	{
 		len++;
