3f567a32a7050325693369440918dbf17fc47d0d
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-25 17:36:31 -0700

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -12,7 +12,7 @@ size_t free_listint_safe(listint_t **h)
 	int diff;
 	listint_t *temp;
 
-	if (!h ||!(*h))
+	if (!h || !(*h))
 	{
 		return (0);
 	}
@@ -35,5 +35,5 @@ size_t free_listint_safe(listint_t **h)
 		}
 	}
 	*h = NULL;
-       return (len);
+	return (len);
 }
