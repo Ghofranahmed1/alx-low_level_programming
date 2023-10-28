09757c2d8cc65322b849487f461fd0286c4bd938
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-27 17:27:51 -0700

    101

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - prog checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	/*00000000 00000000 00000000 00000001.*/
+	unsigned int x = 1;
+	char *c;
+	/*When we do (char*)&x, we’re typecasting the address
+	 *  of x to a char pointer. This tells the compiler 
+	 *  to treat the address of x as if it’s the address of a char
+	 *Since c is a char pointer, it’s pointing to a single byte.
+	 */
+	c = (char*) &x;
+	/*(int)*c: This is casting that value to an integer.*/
+
+	return ((int)*c);
+}
