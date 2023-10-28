202af85f39160bf0a0ab7d7b104af67bd919b80d
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-27 17:29:28 -0700

    101

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,16 +7,21 @@
  */
 int get_endianness(void)
 {
-	/*00000000 00000000 00000000 00000001.*/
+	/*
+	 * 00000000 00000000 00000000 00000001.
+	 */
 	unsigned int x = 1;
 	char *c;
-	/*When we do (char*)&x, we’re typecasting the address
-	 *  of x to a char pointer. This tells the compiler 
+	/*
+	 * When we do (char*)&x, we’re typecasting the address
+	 *  of x to a char pointer. This tells the compiler
 	 *  to treat the address of x as if it’s the address of a char
 	 *Since c is a char pointer, it’s pointing to a single byte.
 	 */
-	c = (char*) &x;
-	/*(int)*c: This is casting that value to an integer.*/
+	c = (char *) &x;
+	/*
+	 * (int)*c: This is casting that value to an integer.
+	 */
 
 	return ((int)*c);
 }
