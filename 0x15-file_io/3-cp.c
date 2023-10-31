cfd80215fac251d32053e980693b9a8c830d2ba9
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-31 15:26:00 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	ssize_t read_byte, written_byte;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),exit (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((read_byte = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		written_byte = write(fd_to, buffer, read_byte);
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		close(fd_from), close(fd_to), exit(99);
 	}
 	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from),exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	}
