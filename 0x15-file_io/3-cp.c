00e56306a3db0b0910ada4b12b702f25080e21aa
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-31 15:27:53 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,6 +30,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from), close(fd_to), exit(99);
 	}
+	}
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
