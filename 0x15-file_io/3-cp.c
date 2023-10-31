37965f647064c317d77e85c830390405e53b7fdb
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-31 15:29:26 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,6 +35,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	}
 	return (0);
 }
