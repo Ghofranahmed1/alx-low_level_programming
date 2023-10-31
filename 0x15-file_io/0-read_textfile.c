b39ec25c1a50f60253b94f7171adcf0da53dbccb
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-31 16:39:53 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 /**
  * read_textfile - function that reads a text file
  * and prints it to the POSIX standard output.
- * @filename - pointer to the file
- * @letters - number of letters it should read and print
+ * @filename: pointer to the file
+ * @letters: number of letters it should read and print
  * Return:  the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -17,24 +17,24 @@ return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-       buffer = malloc (sizeof(char) * letters);
-       if (buffer == NULL)
-	       return (0);
-       read_bytes = read(fd, buffer, letters);
-       if (read_bytes == -1)
-       {
-	       free (buffer);
-	       close(fd);
-	       return (0);
-       }
-       written_bytes = write (STDOUT_FILENO, buffer, read_bytes);
-       if (written_bytes == -1 || written_bytes != read_bytes)
-       {
-	       free (buffer);
-	       close (fd);
-	       return (0);
-       }
-       free (buffer);
-       close (fd);
-       return (written_bytes);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+read_bytes = read(fd, buffer, letters);
+if (read_bytes == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+if (written_bytes == -1 || written_bytes != read_bytes)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (written_bytes);
 }
