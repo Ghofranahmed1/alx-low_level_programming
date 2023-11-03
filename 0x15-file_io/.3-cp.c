e72481d8c379547d325a27b605ce1208aeac3ae3
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-03 16:33:33 -0700

    3

---
+++ 0x15-file_io/.3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * main - function the copey contant of  one file to anothe
+ * @argc: argumen count
+ * @argv: argument vector
+ * Return: 0 on success, or a positive integer on failure
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t read_byte, written_byte;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((read_byte = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (read_byte == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+			exit(98);
+		}
+		written_byte = write(fd_to, buffer, read_byte);
+		if (written_byte != read_byte)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from), close(fd_to), exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	return (0);
+}
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,51 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <elf.h>
 
-void display_error(const char* message) {
-  fprintf(stderr, "%s\n", message);
-  exit(98);
+void read_elf_header(int fd, Elf64_Ehdr *header) {
+	int i, fd;
+    if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+        perror("read");
+        exit(98);
+    }
 }
 
-void display_elf_header(const char* filename) {
-  int fd = open(filename, O_RDONLY);
-  if (fd == -1) {
-  display_error("Failed to open the file");
-  }
-
-  Elf64_Ehdr elf_header;
-  if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
-  display_error("Failed to read the ELF header");
-  }
-
-  // Check if it's a valid ELF file
-  if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-  elf_header.e_ident[EI_MAG2] != ELFMAG2 || elf_header.e_ident[EI_MAG3] != ELFMAG3) {
-  display_error("Not a valid ELF file");
-  }
-
-  // Display the required information
-  printf("Magic: %02x %02x %02x %02x\n", elf_header.e_ident[EI_MAG0], elf_header.e_ident[EI_MAG1],
-  elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
-  printf("Class: %d\n", elf_header.e_ident[EI_CLASS]);
-  printf("Data: %d\n", elf_header.e_ident[EI_DATA]);
-  printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-  printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
-  printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
-  printf("Type: %d\n", elf_header.e_type);
-  printf("Entry point address: %lx\n", elf_header.e_entry);
-
-  close(fd);
+void validate_elf_file(Elf64_Ehdr *header) {
+    if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+        header->e_ident[EI_MAG1] != ELFMAG1 ||
+        header->e_ident[EI_MAG2] != ELFMAG2 ||
+        header->e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Error: not an ELF file\n");
+        exit(98);
+    }
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-  display_error("Usage: elf_header elf_filename");
-  }
+void display_elf_header(Elf64_Ehdr *header) {
+    printf("Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", header->e_ident[i]);
+    }
+    printf("\nClass:                             ");
+    printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+    printf("Data:                              ");
+    printf("%s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+    printf("Version:                           ");
+    printf("%u\n", header->e_ident[EI_VERSION]);
+    printf("OS/ABI:                            ");
+    printf("%u\n", header->e_ident[EI_OSABI]);
+    printf("ABI Version:                       ");
+    printf("%u\n", header->e_ident[EI_ABIVERSION]);
+    printf("Type:                              ");
+    printf("%u\n", header->e_type);
+    printf("Entry point address:               ");
+    printf("%#010x\n", (unsigned int)header->e_entry);
+}
 
-  display_elf_header(argv[1]);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: elf_header elf_filename\n");
+        return 98;
+    }
 
-  return 0;
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 98;
+    }
+
+    Elf64_Ehdr header;
+    read_elf_header(fd, &header);
+    validate_elf_file(&header);
+    display_elf_header(&header);
+
+    close(fd);
+    return 0;
 }
