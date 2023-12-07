b5674568f1130380283e9ac8a430b08d2a0bf8ed
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-12-07 13:36:27 -0800

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,34 +1,25 @@
 #include "lists.h"
-
 /**
  * delete_dnodeint_at_index - function that deletes the node at index
  * @head: my linl list
  * @index: s the index of the node that should be deleted. Index starts at 0
  * Return: Returns: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int len = 0, i;
-	dlistint_t *temp = *head; 
+	dlistint_t *temp = *head;
 
-	if (*head == NULL)
-		return (-1);
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		len++;
 		temp = temp->next;
 	}
-	if (index > (len - 1))
+	if (index > (len - 1) || *head == NULL)
 		return (-1);
 	temp = *head;
 	if (index == 0)
 	{
-		if (temp->next == 0)
-		{
-			*head = NULL;
-			free(temp);
-		}
 		temp->next->prev = NULL;
 		*head = temp->next;
 		temp->next = NULL;
@@ -52,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	temp->next->prev = temp->prev;
 	temp->next = NULL;
 	temp->prev = NULL;
-	free (temp);
+	free(temp);
 	return (1);
 	}
 }
