2496fe853a3e460d3f83f479f43f646baa4bd2f7
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-12-07 09:59:43 -0800

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len -  function that returns the number of elements in a linked list
+ * dlistint_len -  function returns the number of items in linked list
  * @h: my linked list
  * Return: number of elements in my linked list
  */
@@ -13,7 +13,7 @@ size_t dlistint_len(const dlistint_t *h)
 
 	if (h == NULL)
 	{
-		dprintf(2,"Error: empty list.");
+		dprintf(2, "Error: empty list.");
 		return (EXIT_FAILURE);
 	}
 	while (temp != NULL)
